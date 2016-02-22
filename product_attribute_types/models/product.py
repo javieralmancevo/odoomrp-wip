1e2a13722d4af8f218e535681897c0281352e4ef
Javier Almancevo <javieralmancevo@gmail.com>
2016-02-22 19:56:58 +0100

    Removed 'default' and 'required' from product_attribute_types module.

--- product_attribute_types/models/product.py
+++ product_attribute_types/models/product.py
@@ -17,23 +17,9 @@ class ProductAttribute(models.Model):
 class ProductAttributeLine(models.Model):
     _inherit = "product.attribute.line"
 
-    required = fields.Boolean(
-        string='Required', default=True)
-    default = fields.Many2one(
-        comodel_name='product.attribute.value', string='Default')
     attr_type = fields.Selection(
         string='Attribute Type', store=False,
         related='attribute_id.attr_type')
-    
-    @api.onchange('attribute_id', 'value_ids')
-    def automatic_set_default(self):
-        if self.default and self.default.attribute_id == self.attribute_id:
-            return
-        
-        if len(self.value_ids) > 0:
-            self.default = self.value_ids[0]
-        else:
-            self.default = False
 
 
 class ProductAttributeValue(models.Model):
